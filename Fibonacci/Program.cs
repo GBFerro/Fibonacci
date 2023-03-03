857859d6045ce8ba3b1d7227a6a1e1f4bee6e34b
adm <adm@DESKTOP-MO3D261>
2023-03-03 14:05:28 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int ant, atual, posicao;
+
+        Console.WriteLine("Digite a posição da sequencia que quer ver: ");
+        posicao = int.Parse(Console.ReadLine());
+
+        atual = 1;
+        ant = 1;
+
+        for (int i = posicao; i > 0; i--)
+        {
+
+            if ((i == posicao) || (i == posicao - 1))
+            {
+                Console.Write(atual + " | ");
+            }
+            else
+            {
+                Console.Write(atual + ant + " | ");
+                atual += ant;
+                ant = atual - ant;
+
+            }
+        }
+    }
+}
